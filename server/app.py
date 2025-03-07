4c33fa74cdc876b46889b6a96d8a11785153de75
petaboss2000 <petaboss2000@gmail.com>
2025-03-07 13:54:01 +0300

    some

--- server/app.py
+++ server/app.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 
+from starlette.middleware.cors import CORSMiddleware
 
 from server.routers.apps import router as app_router
 from server.routers.store import router as store_router
 from server.routers.users import router as users_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(app_router)
 app.include_router(store_router)
 app.include_router(users_router)
-
-
